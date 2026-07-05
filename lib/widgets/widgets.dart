06313c0f2594966db49cc76bbdbbace2b3ff1f9e
jay4ty410 <wysiwyg5011@gmail.com>
2026-07-05 08:29:46 +0300

    feat(widgets): add shared widget barrel exports

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,5 @@
+export '../core/widgets/custom_button.dart';
+export '../core/widgets/custom_textfield.dart';
+export '../core/widgets/custom_card.dart';
+export '../core/widgets/empty_state.dart';
+export '../core/widgets/loading_widget.dart';
